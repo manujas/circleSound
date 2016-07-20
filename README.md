6d5fe7f7c570f41760930baa9419bf30de8af63c
manujas <arias.emanuel@gmail.com>
2016-07-20 14:25:37 -0300

    todo el juego

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # circleSound
-Jugando con la librería Howler JS
+Jugando con sonidos y formas en javaScript.
+
+Al abrir index.html cada tecla que es precionada generará un circulo de color en una pocición aleatorea, con un color y sonido definidos.
+
+
+
+### Librerías usadas
+[HowlerJS](https://howlerjs.com/)
+[PaperJS](http://paperjs.org/)
---
+++ assets/js/datosTecla.js
@@ -0,0 +1,159 @@
+// objeto con todos los datos necesarios para cada una de las letras del teclado
+var datosTecla = {
+	q: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/bubbles.mp3']
+		}),
+		color: '#1abc9c'
+	},
+	w: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/clay.mp3']
+		}),
+		color: '#2ecc71'
+	},
+	e: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/confetti.mp3']
+		}),
+		color: '#3498db'
+	},
+	r: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/corona.mp3']
+		}),
+		color: '#9b59b6'
+	},
+		t: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/dotted-spiral.mp3']
+		}),
+		color: '#34495e'
+	},
+	y: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/flash-1.mp3']
+		}),
+		color: '#16a085'
+	},
+	u: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/flash-2.mp3']
+		}),
+		color: '#27ae60'
+	},
+	i: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/flash-3.mp3']
+		}),
+		color: '#2980b9'
+	},
+	o: {
+		sonido: new Howl({
+			urls: ['assets/sonidos/glimmer.mp3']
+		}),
+		color: '#8e44ad'
+	},
+	p: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/moon.mp3']
+		}),
+		color: '#2c3e50'
+	},
+	a: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/pinwheel.mp3']
+		}),
+		color: '#f1c40f'
+	},
+	s: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/piston-1.mp3']
+		}),
+		color: '#e67e22'
+	},
+		d: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/piston-2.mp3']
+		}),
+		color: '#e74c3c'
+	},
+	f: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/prism-1.mp3']
+		}),
+		color: '#95a5a6'
+	},
+	g: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/prism-2.mp3']
+		}),
+		color: '#f39c12'
+	},
+	h: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/prism-3.mp3']
+		}),
+		color: '#d35400'
+	},
+	j: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/splits.mp3']
+		}),
+		color: '#1abc9c'
+	},
+	k: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/squiggle.mp3']
+		}),
+		color: '#2ecc71'
+	},
+	l: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/strike.mp3']
+		}),
+		color: '#3498db'
+	},
+	z: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/suspension.mp3']
+		}),
+		color: '#9b59b6'
+	},
+	x: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/timer.mp3']
+		}),
+		color: '#34495e'
+	},
+	c: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/ufo.mp3']
+		}),
+		color: '#16a085'
+	},
+	v: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/veil.mp3']
+		}),
+		color: '#27ae60'
+	},
+	b: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/wipe.mp3']
+		}),
+		color: '#2980b9'
+	},
+	n: {
+		sonido: new Howl({
+			urls: ['assets/sonidos/zig-zag.mp3']
+		}),
+		color: '#8e44ad'
+	},
+	m: {
+		sonido: new Howl({
+  		urls: ['assets/sonidos/moon.mp3']
+		}),
+		color: '#2c3e50'
+	}
+}
